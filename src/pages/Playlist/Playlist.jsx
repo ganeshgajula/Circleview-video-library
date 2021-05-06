f7440be2ef210e0e3fb79aa94380d8c9586bf167
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:22:13 +0530

    add: playlist page

---
+++ src/pages/Playlist/Playlist.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components";
+
+export const Playlist = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Inside Playlists</h1>
+      </div>
+    </>
+  );
+};
