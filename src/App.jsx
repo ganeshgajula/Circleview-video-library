e5941968db79a23c520e93df1e65d1bef1c30b8f
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:08:35 +0530

    add: route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home, Explore, Liked, WatchLater, Playlist } from "./pages";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/likedvideos" element={<Liked />} />
+        <Route path="/watchlater" element={<WatchLater />} />
+        <Route path="/playlist" element={<Playlist />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
