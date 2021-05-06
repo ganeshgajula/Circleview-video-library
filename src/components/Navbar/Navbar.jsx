cb4458fd91845d816d966db2649ade67b8968b50
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:05:11 +0530

    add: link components for navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Navbar/Navbar.css";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
@@ -8,11 +9,21 @@ export const Navbar = () => {
         <h1>finview</h1>
 
         <ul className="nav-list">
-          <li className="nav-item">Home</li>
-          <li className="nav-item">Explore</li>
-          <li className="nav-item">Liked videos</li>
-          <li className="nav-item">Watch later</li>
-          <li className="nav-item">Playlist</li>
+          <Link to="/">
+            <li className="nav-item">Home</li>
+          </Link>
+          <Link to="/explore">
+            <li className="nav-item">Explore</li>
+          </Link>
+          <Link to="/likedvideos">
+            <li className="nav-item">Liked videos</li>
+          </Link>
+          <Link to="/watchlater">
+            <li className="nav-item">Watch later</li>
+          </Link>
+          <Link to="/playlist">
+            <li className="nav-item">Playlist</li>
+          </Link>
         </ul>
       </nav>
     </>
