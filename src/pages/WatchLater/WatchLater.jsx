f5bdf9fa17876c5d151f45b53954cd559ee16f0a
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:23:00 +0530

    add: watchlater page

---
+++ src/pages/WatchLater/WatchLater.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components";
+
+export const WatchLater = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Inside Watchlater</h1>
+      </div>
+    </>
+  );
+};
