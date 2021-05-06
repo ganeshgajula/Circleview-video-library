a2023f903f25e13533583f0181b866d4593c4e04
ganesh <ganeshgajula22@gmail.com>
2021-05-06 23:34:06 +0530

    add:video id route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Home, Explore, Liked, WatchLater, Playlist } from "./pages";
+import { Home, Explore, Liked, WatchLater, Playlist, VideoPage } from "./pages";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
+        <Route path="/watch/:videoID" element={<VideoPage />} />
         <Route path="/likedvideos" element={<Liked />} />
         <Route path="/watchlater" element={<WatchLater />} />
         <Route path="/playlist" element={<Playlist />} />
