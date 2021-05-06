2e7e5c5ce8171cbe71677943afb312b13491cbc7
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:19:23 +0530

    add: home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components";
+
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Inside Home</h1>
+      </div>
+    </>
+  );
+};
