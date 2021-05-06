938067852f8aae658df7b3d386720f0e721f5b20
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:21:14 +0530

    add: liked page

---
+++ src/pages/Liked/Liked.jsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../../components";
+
+export const Liked = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Inside Liked videos</h1>
+      </div>
+    </>
+  );
+};
