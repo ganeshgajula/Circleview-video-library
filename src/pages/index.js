5491e943c8bd49941ad69d396ff1a889df08612d
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:24:39 +0530

    add: index file for ease of export

---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { Explore } from "./Explore";
+export { Home } from "./Home";
+export { Liked } from "./Liked";
+export { Playlist } from "./Playlist";
+export { WatchLater } from "./WatchLater";
