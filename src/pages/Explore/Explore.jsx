b461f4632caee035964c8a9beef5d9c6293783be
ganesh <ganeshgajula22@gmail.com>
2021-05-06 12:20:24 +0530

    add: explore page

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navbar } from "../../components";
+
+export const Explore = () => {
+  return (
+    <>
+      <Navbar />
+      <div style={{ marginTop: "8rem" }}>
+        <h1>Inside Explore</h1>
+      </div>
+    </>
+  );
+};
