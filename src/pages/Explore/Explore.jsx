1c9a1df23d60bb9dd3cf5fca20f8b3a7ee205a12
ganesh <ganeshgajula22@gmail.com>
2021-05-27 09:40:49 +0530

    feat: search

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,38 +1,34 @@
 import React from "react";
 import { Navbar } from "../../components";
 import { useVideos } from "../../context";
-import "./Explore.css";
 import { Link } from "react-router-dom";
+import { VideoCard } from "../../components/VideoCard/VideoCard";
+import "./Explore.css";
 
 export const Explore = () => {
-  const { data } = useVideos();
+  const {
+    data: { videos, searchedKeyword },
+  } = useVideos();
+
+  const filterBySearch = (videosBeforeFilter) =>
+    videosBeforeFilter.filter((video) =>
+      video.name.toLowerCase().includes(searchedKeyword.toLowerCase())
+    );
+
+  const filteredData = filterBySearch(videos);
 
   return (
     <>
       <Navbar />
       <main className="videos-section">
-        {data.videos.map(
-          ({ videoId, name, channelName, level, language, thumbnail }) => (
-            <Link
-              to={`/watch/${videoId}`}
-              style={{ textDecoration: "none", color: "#333" }}
-            >
-              <div className="videos-thumbnail-wrapper" key={videoId}>
-                <img
-                  src={thumbnail}
-                  alt="video-thumbnail"
-                  className="thumbnail-image"
-                />
-                <p className="video-name">{name}</p>
-                <p className="channel-name">{channelName}</p>
-                <div className="more-info">
-                  <span className="level">{level}</span>
-                  <span className="language">{language}</span>
-                </div>
-              </div>
-            </Link>
-          )
-        )}
+        {filteredData.map((video) => (
+          <Link
+            to={`/watch/${video.videoId}`}
+            style={{ textDecoration: "none", color: "#333" }}
+          >
+            <VideoCard {...video} />
+          </Link>
+        ))}
       </main>
     </>
   );
