0e5f1e26d13384a7c919b8e0b6867dce49738677
ganesh <ganeshgajula22@gmail.com>
2021-05-28 22:48:57 +0530

    add: check svg

--- src/components/ReusableSvgs/index.js
+++ src/components/ReusableSvgs/index.js
@@ -1,1 +1,2 @@
 export { SearchSvg } from "./svgs";
+export { CheckSvg } from "./svgs";
--- src/components/ReusableSvgs/svgs.js
+++ src/components/ReusableSvgs/svgs.js
@@ -11,3 +11,12 @@ export const SearchSvg = () => (
     ></path>
   </svg>
 );
+
+export const CheckSvg = () => (
+  <svg width="1rem" height="1rem" viewBox="0 0 24 24" className="verified-icon">
+    <path
+      d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10s10-4.5 10-10S17.5 2 12 2m-2 15l-5-5l1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
