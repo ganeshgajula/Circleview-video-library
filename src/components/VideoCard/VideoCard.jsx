010a1222bb832dd6a79596f6a3f3a57e129a5ef7
ganesh <ganeshgajula22@gmail.com>
2021-05-28 23:01:04 +0530

    add: channel image and styling

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import { CheckSvg } from "../ReusableSvgs";
 import "./VideoCard.css";
 
 export const VideoCard = ({
-  videoId,
   name,
   channelName,
   level,
   language,
   thumbnail,
+  channelLogo,
 }) => {
   return (
-    <div className="videos-thumbnail-wrapper" key={videoId}>
+    <div className="videos-thumbnail-wrapper">
       <img src={thumbnail} alt="video-thumbnail" className="thumbnail-image" />
-      <p className="video-name">{name}</p>
-      <p className="channel-name">{channelName}</p>
-      <div className="more-info">
-        <span className="level">{level}</span>
-        <span className="language">{language}</span>
+      <div className="videoInfo-container">
+        <img src={channelLogo} alt="channel-logo" className="channel-image" />
+        <div className="videoDetails-section">
+          <p className="video-name">{name}</p>
+          <div className="channel-details">
+            <span className="channel-name">{channelName}</span>
+            <CheckSvg />
+          </div>
+          <div className="more-info">
+            <span className="level">{level}</span> •
+            <span className="language">{language}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
