c4f43a4c44949001ec520e40957ca6deefc95003
ganesh <ganeshgajula22@gmail.com>
2021-05-29 13:10:26 +0530

    add: video details and action btns

--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import ReactPlayer from "react-player/youtube";
 import { useParams } from "react-router-dom";
 import { Navbar } from "../../components";
+import {
+  BookmarkSvg,
+  CheckSvg,
+  WatchLaterSvg,
+  HeartSvg,
+  PlaylistPlusSvg,
+} from "../../components/ReusableSvgs";
 import { useVideos } from "../../context";
 import "./VideoPage.css";
 
@@ -21,9 +28,44 @@ export const VideoPage = () => {
         <ReactPlayer
           url={`https://youtube.com/embed/${requestedVideo.videoId}`}
           controls
-          width={`1080px`}
-          height={`540px`}
+          width="100%"
+          height="100%"
         />
+        <div className="video-details">
+          <span className="channel-view">
+            <img
+              src={requestedVideo.channelLogo}
+              alt="channelLogo"
+              className="channel-image"
+            />
+            <span className="channel-info">
+              <div className="channel-details">
+                <h2 className="channel-name">{requestedVideo.channelName}</h2>
+                <CheckSvg />
+              </div>
+              <small className="subscribers-count">
+                {requestedVideo.subscriberCount} subscribers
+              </small>
+            </span>
+          </span>
+
+          <span className="video-action-btns">
+            <button className="video-action-btn">
+              <BookmarkSvg />
+            </button>
+            <button className="video-action-btn">
+              <WatchLaterSvg />
+            </button>
+            <button className="video-action-btn">
+              <HeartSvg />
+            </button>
+            <button className="video-action-btn">
+              <PlaylistPlusSvg />
+            </button>
+          </span>
+        </div>
+        <h1 className="video-title">{requestedVideo.name}</h1>
+        <p className="video-description">{requestedVideo.description}</p>
       </div>
     </>
   );
