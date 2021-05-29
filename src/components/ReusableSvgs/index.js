18e0e15667d8f18cf08d6af391a09a7618d754f0
ganesh <ganeshgajula22@gmail.com>
2021-05-29 13:12:19 +0530

    add: file handling

--- src/components/ReusableSvgs/index.js
+++ src/components/ReusableSvgs/index.js
@@ -2,4 +2,5 @@ export { SearchSvg } from "./svgs";
 export { CheckSvg } from "./svgs";
 export { BookmarkSvg } from "./svgs";
 export { WatchLaterSvg } from "./svgs";
+export { HeartSvg } from "./svgs";
 export { PlaylistPlusSvg } from "./svgs";
