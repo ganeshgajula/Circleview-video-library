a35df464af9afed59c4c11e1faaa758e56cf852e
ganesh <ganeshgajula22@gmail.com>
2021-05-29 12:56:06 +0530

    add: svgs

--- src/components/ReusableSvgs/index.js
+++ src/components/ReusableSvgs/index.js
@@ -1,2 +1,5 @@
 export { SearchSvg } from "./svgs";
 export { CheckSvg } from "./svgs";
+export { BookmarkSvg } from "./svgs";
+export { WatchLaterSvg } from "./svgs";
+export { PlaylistPlusSvg } from "./svgs";
--- src/components/ReusableSvgs/svgs.js
+++ src/components/ReusableSvgs/svgs.js
@@ -20,3 +20,59 @@ export const CheckSvg = () => (
     ></path>
   </svg>
 );
+
+export const BookmarkSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="bookmark-icon"
+  >
+    <path
+      d="M17 18l-5-2.18L7 18V5h10m0-2H7a2 2 0 0 0-2 2v16l7-3l7 3V5a2 2 0 0 0-2-2z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const WatchLaterSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="clock-outline-icon"
+  >
+    <path
+      d="M12 20a8 8 0 0 0 8-8a8 8 0 0 0-8-8a8 8 0 0 0-8 8a8 8 0 0 0 8 8m0-18a10 10 0 0 1 10 10a10 10 0 0 1-10 10C6.47 22 2 17.5 2 12A10 10 0 0 1 12 2m.5 5v5.25l4.5 2.67l-.75 1.23L11 13V7h1.5z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const HeartSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="heart-outline-svg"
+  >
+    <path
+      d="M12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const PlaylistPlusSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="playlist-plus-svg"
+  >
+    <path
+      d="M2 16h8v-2H2m16 0v-4h-2v4h-4v2h4v4h2v-4h4v-2m-8-8H2v2h12m0 2H2v2h12v-2z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
