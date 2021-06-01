2c1996a75c70ced16a9b3c1e54088c29321c9a04
ganesh <ganeshgajula22@gmail.com>
2021-06-01 22:55:29 +0530

    refactor: route path

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/watch/:videoID" element={<VideoPage />} />
+        <Route path="/watch/:videoId" element={<VideoPage />} />
         <Route path="/likedvideos" element={<Liked />} />
         <Route path="/watchlater" element={<WatchLater />} />
         <Route path="/playlist" element={<Playlist />} />
