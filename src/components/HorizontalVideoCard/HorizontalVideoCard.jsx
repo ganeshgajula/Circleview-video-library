acae93f6a08eb20e07539b9cbf75d72d4aad6788
ganesh <ganeshgajula22@gmail.com>
2021-06-02 23:51:54 +0530

    add: horizontal video card

---
+++ src/components/HorizontalVideoCard/HorizontalVideoCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { CheckSvg } from "../ReusableSvgs";
+import "./HorizontalVideoCard.css";
+
+export const HorizontalVideoCard = ({
+  name,
+  channelName,
+  thumbnail,
+  description,
+  channelLogo,
+}) => {
+  return (
+    <>
+      <div className="horizontal-video-card">
+        <img
+          src={thumbnail}
+          alt="thumbnail"
+          className="selected-video-thumbnail"
+        />
+        <div className="video-details-vertical">
+          <h2 className="video-name">{name}</h2>
+          <div className="channel-details">
+            <img
+              src={channelLogo}
+              alt="creator"
+              className="channel-logo-playlist-card"
+            />
+            <span className="creator-name">{channelName}</span>
+            <CheckSvg />
+          </div>
+          <p className="video-info">{description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/components/HorizontalVideoCard/index.js
@@ -0,0 +1,1 @@
+export { HorizontalVideoCard } from "./HorizontalVideoCard";
