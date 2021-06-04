30a22db8553ec11b7fd93efd05ac11f4ba8f3c47
ganesh <ganeshgajula22@gmail.com>
2021-06-04 15:16:09 +0530

    add: svgs

--- src/components/ReusableSvgs/index.js
+++ src/components/ReusableSvgs/index.js
@@ -1,7 +1,10 @@
 export { SearchSvg } from "./svgs";
 export { CheckSvg } from "./svgs";
 export { BookmarkSvg } from "./svgs";
+export { BookmarkOutlinedSvg } from "./svgs";
+export { WatchLaterOutlinedSvg } from "./svgs";
 export { WatchLaterSvg } from "./svgs";
+export { HeartOutlinedSvg } from "./svgs";
 export { HeartSvg } from "./svgs";
 export { PlaylistPlusSvg } from "./svgs";
 export { PencilSvg } from "./svgs";
--- src/components/ReusableSvgs/svgs.js
+++ src/components/ReusableSvgs/svgs.js
@@ -23,12 +23,12 @@ export const CheckSvg = () => (
   </svg>
 );
 
-export const BookmarkSvg = () => (
+export const BookmarkOutlinedSvg = () => (
   <svg
     width="1.6rem"
     height="1.6rem"
     viewBox="0 0 24 24"
-    className="bookmark-icon"
+    className="bookmark-outlined-icon"
   >
     <path
       d="M17 18l-5-2.18L7 18V5h10m0-2H7a2 2 0 0 0-2 2v16l7-3l7 3V5a2 2 0 0 0-2-2z"
@@ -37,7 +37,21 @@ export const BookmarkSvg = () => (
   </svg>
 );
 
-export const WatchLaterSvg = () => (
+export const BookmarkSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="bookmark-icon"
+  >
+    <path
+      d="M17 3H7a2 2 0 0 0-2 2v16l7-3l7 3V5a2 2 0 0 0-2-2z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const WatchLaterOutlinedSvg = () => (
   <svg
     width="1.6rem"
     height="1.6rem"
@@ -51,7 +65,21 @@ export const WatchLaterSvg = () => (
   </svg>
 );
 
-export const HeartSvg = () => (
+export const WatchLaterSvg = () => (
+  <svg
+    width="1.6rem"
+    height="1.6rem"
+    viewBox="0 0 24 24"
+    className="clock-icon"
+  >
+    <path
+      d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2m4.2 14.2L11 13V7h1.5v5.2l4.5 2.7l-.8 1.3z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const HeartOutlinedSvg = () => (
   <svg
     width="1.6rem"
     height="1.6rem"
@@ -60,6 +88,15 @@ export const HeartSvg = () => (
   >
     <path
       d="M12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3z"
+      fill="currentColor"
+    ></path>
+  </svg>
+);
+
+export const HeartSvg = () => (
+  <svg width="1.6rem" height="1.6rem" viewBox="0 0 24 24" className="heart-svg">
+    <path
+      d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5C2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53L12 21.35z"
       fill="currentColor"
     ></path>
   </svg>
