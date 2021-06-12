c063bcf2f35013a35c2e3d776def0ecc2ef49c90
ganesh <ganeshgajula22@gmail.com>
2021-06-12 16:46:43 +0530

    add: landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
 import { Navbar } from "../../components";
+import { Link } from "react-router-dom";
+import heroImg from "../../assets/circleview-landing-img.jpg";
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <>
+    <header
+      className="hero"
+      style={{ background: `url(${heroImg}) no-repeat center top/cover` }}
+    >
       <Navbar />
-      <div style={{ marginTop: "8rem" }}>
-        <h1>Inside Home</h1>
+      <div className="content">
+        <div className="hero-title">Investing is a long term game</div>
+        <p className="hero-description">
+          Master the art of personal finance & investing from the specially
+          curated and handpicked videos.
+        </p>
+        <Link to="/explore">
+          <button className="btn-primary btn-md">Get Started</button>
+        </Link>
       </div>
-    </>
+    </header>
   );
 };
