3668641c6d983d110c82cc06b64c20e1c46142bb
ganesh <ganeshgajula22@gmail.com>
2021-06-15 15:02:59 +0530

    feat: user persistence

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -2,29 +2,28 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 
 export const AuthContext = createContext();
 
-const user = {
-  username: "ganesh",
-  password: "gajula",
-};
-
 export const AuthProvider = ({ children }) => {
   const [isUserLoggedIn, setLogin] = useState(false);
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState("");
 
   useEffect(() => {
-    const loginStatus = JSON.parse(localStorage.getItem("login"));
+    const loginStatus = JSON.parse(localStorage.getItem("userInfo"));
     loginStatus?.isUserLoggedIn && setLogin(true);
+    loginStatus?.userId && setUserId(loginStatus.userId);
+    loginStatus?.username && setUsername(loginStatus.username);
   }, []);
-
-  const loginUserWithCredentials = (username, password) => {
-    if (user.username === username && user.password === password) {
-      setLogin(true);
-      localStorage.setItem("login", JSON.stringify({ isUserLoggedIn: true }));
-    }
-  };
 
   return (
     <AuthContext.Provider
-      value={{ isUserLoggedIn, setLogin, loginUserWithCredentials }}
+      value={{
+        isUserLoggedIn,
+        username,
+        userId,
+        setLogin,
+        setUsername,
+        setUserId,
+      }}
     >
       {children}
     </AuthContext.Provider>
