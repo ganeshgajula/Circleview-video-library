41a12df0a9c489d91e4e12b58a4ef4c8f6a1cd6e
ganesh <ganeshgajula22@gmail.com>
2021-06-22 00:06:16 +0530

    refactor: watch later videos

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,32 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { Navbar } from "../../components";
 import { useVideos } from "../../context";
 import { HorizontalVideoCard } from "../../components";
 import "./WatchLater.css";
+import { getPlaylistByName } from "../../utils";
 
 export const WatchLater = () => {
   const {
     data: { playlist },
   } = useVideos();
 
+  const watchLaterPlaylist = getPlaylistByName(playlist, "Watch later");
+
   return (
     <>
       <Navbar />
       <h1 className="video-playlist-title">Watch later</h1>
       <div className="selected-playlist-videos">
-        {playlist.map(
-          (playlist) =>
-            playlist.name === "Watch later" &&
-            playlist.videos.map((video) => (
-              <Link
-                key={video._id}
-                to={`/watch/${video._id}`}
-                style={{ textDecoration: "none", color: "#000" }}
-              >
-                <HorizontalVideoCard key={video._id} {...video} />
-              </Link>
-            ))
+        {watchLaterPlaylist.videos.length === 0 ? (
+          <h3 className="empty-playlist-message">
+            No videos added to watch later
+          </h3>
+        ) : (
+          watchLaterPlaylist.videos.map((video) => (
+            <HorizontalVideoCard
+              key={video._id}
+              playlistId={watchLaterPlaylist._id}
+              {...video}
+            />
+          ))
         )}
       </div>
     </>
