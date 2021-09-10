ff33d1723c0a494f632ec0ec8741af9bc63bf517
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-09-10 16:01:25 +0530

    refactor: error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useAuth, useVideos } from "./context";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
@@ -19,59 +21,59 @@ import "./App.css";
 
 const App = () => {
   const { videosDispatch } = useVideos();
-  const { userId, isUserLoggedIn } = useAuth();
-  useEffect(
-    () => {
+  const { userId, token } = useAuth();
+  useEffect(() => {
+    (async () => {
+      try {
+        const {
+          data: { videos },
+        } = await axios.get("http://localhost:4000/videos");
+        videosDispatch({ type: "LOAD_VIDEOS", payload: videos });
+      } catch (error) {
+        toast.error(error.response.data.errorMessage, {
+          position: "bottom-center",
+          autoClose: 3000,
+        });
+      }
+    })();
+  }, [videosDispatch]);
+
+  useEffect(() => {
+    token &&
       (async () => {
         try {
           const {
-            data: { videos },
-          } = await axios.get("http://localhost:4000/videos");
-          videosDispatch({ type: "LOAD_VIDEOS", payload: videos });
+            data: {
+              playlist: { playlists },
+            },
+          } = await axios.get(
+            `http://localhost:4000/playlists/${userId}/playlist`
+          );
+
+          videosDispatch({ type: "LOAD_PLAYLIST", payload: playlists });
+
+          const {
+            data: {
+              history: { videos },
+            },
+          } = await axios.get(`http://localhost:4000/history/${userId}/videos`);
+
+          videosDispatch({ type: "LOAD_HISTORY", payload: videos });
         } catch (error) {
-          console.error(error);
+          toast.error(error.response.data.errorMessage, {
+            position: "bottom-center",
+            autoClose: 3000,
+          });
         }
       })();
-    }, //eslint-disable-next-line
-    []
-  );
-
-  useEffect(
-    () => {
-      (async () => {
-        if (isUserLoggedIn) {
-          try {
-            const {
-              data: {
-                playlist: { playlists },
-              },
-            } = await axios.get(
-              `http://localhost:4000/playlists/${userId}/playlist`
-            );
-
-            videosDispatch({ type: "LOAD_PLAYLIST", payload: playlists });
-
-            const {
-              data: {
-                history: { videos },
-              },
-            } = await axios.get(
-              `http://localhost:4000/history/${userId}/videos`
-            );
-
-            videosDispatch({ type: "LOAD_HISTORY", payload: videos });
-          } catch (error) {
-            console.error(error);
-          }
-        }
-      })();
-    }, //eslint-disable-next-line
-    [userId]
-  );
+  }, [token, userId, videosDispatch]);
 
   return (
     <div className="App">
+      <ToastContainer />
       <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/watch/:videoId" element={<VideoPage />} />
@@ -83,8 +85,6 @@ const App = () => {
           element={<SelectedPlaylist />}
         />
         <PrivateRoute path="/history" element={<History />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
